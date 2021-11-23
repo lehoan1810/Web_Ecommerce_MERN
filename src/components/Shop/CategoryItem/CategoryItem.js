401a58a0d03a6ccbf827faf81e8ca84303622bcf
puimekster <hoanhao18102000@gmail.com>
2021-11-23 23:17:48 +0700

    filter

--- src/components/Shop/CategoryItem/CategoryItem.js
+++ src/components/Shop/CategoryItem/CategoryItem.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import ItemProduct from "../../Product/ItemProduct/ItemProduct.js";
-import Search from "../../../images/Search.png";
+// import Search from "../../../images/Search.png";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import authHeader from "../../../service/AuthHeader.js";
+import "antd/dist/antd.css";
+import { Slider, Input } from "antd";
+import "./CategoryItem.css";
 
 const CategoryItem = () => {
 	const { id } = useParams();
@@ -16,25 +19,72 @@ const CategoryItem = () => {
 				.get(url, { headers: authHeader() })
 				.then((res) => {
 					setDataProduct(res.data.products);
-					console.log(res.data.products);
+					console.log("dataProduct", res.data.products);
 				})
 				.catch((err) => console.log(err));
 		};
 		loadProduct();
 	}, [url]);
+
+	const [filterInput, setFilterInput] = useState("");
+	const searchText = (e) => {
+		setFilterInput(e.target.value);
+	};
+	const onChange = (value) => {
+		console.log("onChange: ", value);
+		setFilterInput(value);
+	};
+	const [sortPrice, setSortPrice] = useState([]);
+	const maxPrice = () => {
+		setSortPrice(
+			dataProduct.sort((a, b) => {
+				return a.price - b.price;
+			})
+		);
+		console.log(sortPrice);
+	};
+	const filterData = () => {
+		if (filterInput === "") return dataProduct;
+
+		if (isNaN(filterInput)) {
+			return dataProduct.filter(({ name }) => name.includes(filterInput));
+		}
+		return dataProduct.filter(({ price }) => price <= filterInput);
+	};
+
 	return (
 		<div>
 			<div className="menu-product-item">
 				<div className="handel-item-product">
 					<div className="simple-search">
-						<input type="text" placeholder="Type here and hit Enter" />
-						<button>
-							<img className="img-search" src={Search} alt="" />
+						<input
+							onChange={(e) => searchText(e)}
+							type="text"
+							placeholder="Tìm kiếm sản phẩm"
+						/>
+					</div>
+					<div className="change-price">
+						<button className="button-sort" onClick={maxPrice}>
+							Sắp xếp tăng dần
 						</button>
+						<Slider
+							className="range-search"
+							min={500000}
+							max={27000000}
+							step={100000}
+							defaultValue={500000}
+							onChange={onChange}
+						/>
+						<Input
+							className="input-number"
+							style={{ width: "10rem" }}
+							value={filterInput}
+							defaultValue={500000}
+						/>
 					</div>
 				</div>
 				<div className="show-item-product">
-					{dataProduct.map((item, id) => (
+					{filterData().map((item, id) => (
 						<div key={id} className="item-flex">
 							<ItemProduct data={item} />
 						</div>
